3e1e554d5bf698b60219f75e1019c65e972064ee
Roberth <mausjumbo@outlook.es>
2024-02-07 14:21:52 -0500

    This project is the test angular

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service'
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) { }
+
+  get tags(): string[] {
+    return this.gifsService.tagsHistory;
+  }
+
+  searchGifs(tag: string) {
+    this.gifsService.searchTag(tag);
+  }
+}
